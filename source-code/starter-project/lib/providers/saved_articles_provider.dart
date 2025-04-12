64c12f41a7139f07b989bf03f4c06c66603c5dae
josteveadekanbi <adekanbijosteve@gmail.com>
2025-04-12 23:54:19 +0100

    Starter push

---
+++ source-code/starter-project/lib/providers/saved_articles_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:newsify/models/article_model.dart';
+import 'package:newsify/repositories/hive_repository.dart';
+
+final savedArticlesProvider =
+    NotifierProvider<SavedArticlesNotifier, List<Article>>(
+  SavedArticlesNotifier.new,
+);
+
+class SavedArticlesNotifier extends Notifier<List<Article>> {
+  late final HiveRepository hiveRepository;
+  @override
+  List<Article> build() {
+    hiveRepository = ref.read(hiveRespositoryProvider);
+    return hiveRepository.getSavedArticles();
+  }
+
+  void updateArticle(Article article, bool save) {
+    final newState = [...state];
+    hiveRepository.saveArticle(article);
+    newState.add(article);
+    state = newState;
+  }
+}
